051877b26f02bede47feb51f3c22cd9927472384
Konstantin B <konstantin@bashuk.me>
2025-02-21 20:20:43 +0200

    added render functions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
-import iziToast from 'izitoast';
 import "izitoast/dist/css/iziToast.min.css";
 import getImages from './js/pixabay-api';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { imageTemplate, imagesTemplate } from './js/render-functions';
 
 const refs = {
     form: document.querySelector(".search-form"),
@@ -28,37 +27,6 @@ form.addEventListener("submit", (e) => {
             lightbox.refresh();
         });
 });
-
-function imageTemplate(image) {
-    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
-    return `
-        <a class="gallery-item" href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}" data-source="${largeImageURL}" />
-            <div class="image-features">
-                <div class="features-text-field">
-                    <p class="image-feature-text">Likes</p>
-                    <p class="image-feature-text">${likes}</p>
-                </div>
-                <div class="features-text-field">
-                    <p class="image-feature-text">Views</p>
-                    <p class="image-feature-text">${views}</p>
-                </div>
-                <div class="features-text-field">
-                    <p class="image-feature-text">Comments</p>
-                    <p class="image-feature-text">${comments}</p>
-                </div>
-                <div class="features-text-field">
-                    <p class="image-feature-text">Downloads</p>
-                    <p class="image-feature-text">${downloads}</p>
-                </div>
-            </div>
-        </a>
-    `;
-}
-
-function imagesTemplate(images) {
-    return images.map(imageTemplate).join('');
-};
 
 gallery.addEventListener('click', e => {
     e.preventDefault();
